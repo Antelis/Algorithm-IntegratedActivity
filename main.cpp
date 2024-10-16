d26a2cc7679be8bc590c3dd62c7045fdf6f169c9
Goose03 <gusgt03@gmail.com>
2024-10-16 13:00:34 -0600

    Added input.sh

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Usen el input.sh para compilar todo
+// Ahi escriban los comandos que se necesiten para cmake 
+// Se corre haciendo ./input.sh
+
+int main(){
+
+  cout << "Hello World" << endl;
+
+  return 0;
+}
