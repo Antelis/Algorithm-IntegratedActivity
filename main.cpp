aaf4451d12b635aa0c9cdacd51083ba58857222f
Goose03 <gusgt03@gmail.com>
2024-10-16 13:47:44 -0600

    La primera N la necesitamos todos xd

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 
+#include <vector>
+
+#include "distMatrix.h"
+
 // Usen el input.sh para compilar todo
 // Ahi escriban los comandos que se necesiten para cmake 
 // Se corre haciendo ./input.sh
 
 int main(){
 
-  cout << "Hello World" << endl;
+  int N;
+  cin >> N;
 
   return 0;
 }
